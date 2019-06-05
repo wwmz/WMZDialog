dcf5640e16717010b88a85eb66e4aa433fb7b04d
fvbn147 <925457662@qq.com>
2019-06-05 11:14:14 +0800

    Initial Commit

---
+++ WMZDialog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMZDialog
+//
+//  Created by wmz on 2019/6/5.
+//  Copyright © 2019年 wmz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WMZDialog/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WMZDialog
+//
+//  Created by wmz on 2019/6/5.
+//  Copyright © 2019年 wmz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
